ad66d048ad7cc31d16c502312c3ee76485638e07
mitragroups <miftahmuhajir@gmail.com>
2024-01-10 02:04:57 +0700

    Add method show in books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,12 @@ class BooksController < ApplicationController
     render plain: @books.pluck(:title, :price, :page)
   end
 
+  def show
+    id = params[:id]
+    @book = Book.find(id)
+    #render plain: @book.title
+  end
+
   def price_sequence
     @priseq = Book.expensive_sequence
       render json: {
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'books/index'
   get 'books/price_sequence'
   get 'books/termurah'
+  resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
